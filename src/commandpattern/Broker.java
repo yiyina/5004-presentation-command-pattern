1cc2520aeae94a02e73f4c84b6de171413578f14
yiyina <55360195+yiyina@users.noreply.github.com>
2023-04-10 17:44:33 -0700

    Add Javadoc

--- src/commandpattern/Broker.java
+++ src/commandpattern/Broker.java
@@ -1,15 +1,27 @@
 package commandpattern;
+
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * A class representing a broker that can take and place orders.
+ */
 public class Broker {
   private List<Order> orderList = new ArrayList<Order>();
 
-  public void takeOrder(Order order){
+  /**
+   * Takes an order and adds it to the broker's list of orders.
+   *
+   * @param order the order to take
+   */
+  public void takeOrder(Order order) {
     orderList.add(order);
   }
 
-  public void placeOrders(){
+  /**
+   * Places all orders in the broker's list of orders by executing them and then clears the list.
+   */
+  public void placeOrders() {
 
     for (Order order : orderList) {
       order.execute();
